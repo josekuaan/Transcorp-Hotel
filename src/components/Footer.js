c16c6de472db3ff1e4f00ad98e6162860176abbc
Ukachukwu ifanyi <joshuaifeanyi67@gmail.com>
2019-10-16 19:29:43 -0700

    correction at the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,23 +16,23 @@ export default function Footer() {
             </div>
             <div className="col-sm-3">
                 <p>Our Location</p>
-                <p>career</p>
+                <p>Career</p>
                 <p>About </p>
-                <p>ontact </p>
+                <p>Contact </p>
             </div>
             <div className="col-sm-3">
                 <p>Faq</p>
                 <p>Blog</p>
-                <p>car Blog</p>
+                <p>Car Blog</p>
                 <p>Location</p>
-                <p>press</p>
+                <p>Press</p>
             </div>
             <div className="col-sm-3">
                 <p>Signe up to get our newsletter</p>
                 <input type="text" placeholder=" your email..."/>
                 <hr />
             </div>
-            <hr /><br />
+            <hr />
             <div className="row mt-20 social-media">
                 <div className="col-sm-3"><i class="fab fa-facebook-square"></i></div>
                 <div className="col-sm-3"><i class="fab fa-twitter"></i></div>
